f1bad7343b8e53a515585b3eacd10b8a841ae66b
Mohan Das <mohandaskj.11@gmail.com>
2025-12-08 14:43:45 +0530

    Testing Completed!

--- Productivity/Commons/SpinnerTimer.py
+++ Productivity/Commons/SpinnerTimer.py
@@ -7,6 +7,6 @@ def spintimer(seconds):
         sym = next(spinner)
         sys.stdout.write(f"\r{sym}  {mins:02d}:{secs:02d}")
         sys.stdout.flush()
-        time.sleep()
+        time.sleep(1)
     sys.stdout.write("\033[2k\r")
     sys.stdout.flush()
--- Productivity/Time_Blocking/MTTimer.py
+++ Productivity/Time_Blocking/MTTimer.py
@@ -1,5 +1,6 @@
 import Productivity.Commons.SpinnerTimer as stimer
 import winsound,threading
+import Productivity.Time_Blocking.MicroTasker as mt
 
 count = 0
 
@@ -7,16 +8,16 @@ def execute_task(tasks,per_task):
     global count
     target = len(tasks)
 
-    if count < target:
+    if count > target:
         return
     
     temp = tasks.pop(0)
 
     winsound.Beep(1000,500)
     print(f'Current Tasks: {temp}')
-    stimer(per_task)
+    stimer.spintimer(per_task)
     print(f'Completed: {temp}')
     winsound.Beep(1500,700)
 
     if count < target:
-        threading.Timer(2,execute_task(tasks,per_task)).start()
+        threading.Timer(2,execute_task,args=(tasks,per_task)).start()
