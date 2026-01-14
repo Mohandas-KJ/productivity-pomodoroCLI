da754a025bf3603ed153844a788bd2dc600cdd8a
Mohan Das <mohandaskj.11@gmail.com>
2026-01-14 23:35:05 +0530

    Refactor run.py to improve command structure and argument handling

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
+# This Script is to start the CLI Tool
+
+# Imports
 import sys
 import subprocess
-subprocess.run([sys.executable, "src/pomodoro.py"])
+
+# Command template
+cmd = [sys.executable,"src/pomodoro.py"]
+
+# Check if argument is passed
+if len(sys.argv) > 1:
+    cmd.extend(sys.argv[1:])  # Pass it to the file
+
+subprocess.run(cmd)
